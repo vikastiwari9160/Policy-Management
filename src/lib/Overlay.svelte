725c805d000106c024f1b6cc0f0a8aca69291f1c
VIKAS TIWARI <vkt9160@gmail.com>
2024-07-25 16:14:27 +0530

    Overlay bug fix

--- src/lib/Overlay.svelte
+++ src/lib/Overlay.svelte
@@ -4,17 +4,20 @@
 
 <div
     class="w-screen h-screen fixed top-0 left-0 flex justify-center items-center z-10 opacity-60 bg-black"
+></div>
+
+<div
+    class="w-screen h-screen fixed top-0 left-0 flex justify-center items-center z-20"
     on:click={() => {
         isOverlayOpen.set(false);
     }}
 >
     <div
-        class=" bg-gray-900 text-white rounded-md px-8 py-10 relative max-w-lg"
-        role="button"
+        class=" bg-white text-black rounded-md px-8 py-10 relative max-w-lg"
         on:click|stopPropagation
     >
         <button
-            class="absolute top-2 right-3 text-4xl text-gray-300 hover:translate-y-1 transition-transform"
+            class="absolute top-2 right-3 text-4xl hover:translate-y-1 transition-transform"
             on:click={() => {
                 isOverlayOpen.set(false);
             }}>&times;</button
